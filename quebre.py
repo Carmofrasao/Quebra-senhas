88693d04bbaebcd67d25f10b53f64912e0e8b273
ANDERSON APARECIDO DO CARMO FRASAO <aacf20@inf.ufpr.br>
2023-04-25 15:19:25 -0300

    add e1

---
+++ quebre.py
@@ -0,0 +1,16 @@
+import hashlib
+
+chave = 'e8d95a51f3af4a3b134bf6bb680a213a'
+
+arquivo = open("python.txt", "rb")
+
+bytes = arquivo.readlines()
+
+for palavra in bytes:
+    palavra = palavra[0:5]
+    hash = hashlib.md5(palavra).hexdigest()
+
+    if(chave == hash):
+        print("achou")
+        print(palavra)
+        break
